fda2c60db36d93ffea79e1b4974a758371dd5528
YIM-SSOO <81502053+YIM-SSOO@users.noreply.github.com>
2021-09-27 21:44:25 +0900

    File Read, Write

--- src/FIleOutPut.java
+++ src/FIleOutPut.java
@@ -27,17 +27,34 @@ public class FIleOutPut {
 
         //만약 사용자가 입력한 문자열을 파일에 쓰고 싶다면?
         Scanner scanner = new Scanner(System.in);
-        String input = scanner.nextLine();
+        System.out.println("종료하시려면 입력없이 엔터를 눌러주세요!");
 
-        try {
-            writer.write(input);
-           // writer.write("치킨은 살 안쪄요.\n"); // 줄바꿈 \n
-            //writer.write("내가 쪄요.");
-            //FileWriter를 사용할 때는 꼭 다 사용 후 close()를 해줘야한다.
+        while(true) {
+            String input = scanner.nextLine();
+
+            //입력 값이 없으면
+            if(input.equals("")){
+                System.out.println("파일 입력을 종료합니다.");
+                break;
+            }
+
+            try {
+                //입력받은 글은 줄바꿈 처리 
+                writer.write(input + "\n");
+                // writer.write("치킨은 살 안쪄요.\n"); // 줄바꿈 \n
+                //writer.write("내가 쪄요.");
+                //FileWriter를 사용할 때는 꼭 다 사용 후 close()를 해줘야한다.
+
+            } catch (IOException e) {
+                System.out.println("파일에 데이터를 쓰는 데에 실패했습니다.");
+                System.exit(2);
+            }
+        }
+        try{
             writer.close();
-        } catch (IOException e) {
-            System.out.println("파일에 데이터를 쓰는 데에 실패했습니다.");
-            System.exit(2);
+
+        }catch (IOException e){
+            System.out.println("파일 닫기에 실패했습니다.");
         }
     }
 }
