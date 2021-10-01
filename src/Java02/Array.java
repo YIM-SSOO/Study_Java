4710a53701329bced903bbd9e93eed9d5b04d191
YIM-SSOO <81502053+YIM-SSOO@users.noreply.github.com>
2021-10-01 23:55:07 +0900

    Array

--- src/Java02/Array.java
+++ src/Java02/Array.java
@@ -5,6 +5,15 @@ import java.util.Arrays;
 public class Array {
 
     public static void main(String[] args) {
+        // 배열
+
+        // 배열의 메모리 크기
+        // 배열을 구성하는 데이터의 자료형에 따라서 배열의 메모리 크기가 결정된다.
+        int[] arr = new int[3];
+        // => int == 4 byte / int[0] == 4byte == arr = 12byte
+
+
+
         // 배열의 기본 속성
         // 기본 자료형 데이터를 담고 있는 변수와 달리 배열변수는 배열 데이터의 주소를 담고 있다.
 
@@ -24,6 +33,8 @@ public class Array {
         System.out.println("aeeAtt3 : " + Arrays.toString(arrAtt3));
 
         // 배열 레퍼런스
+        //기본 자료형 데이터를 담고 있는 변수와 달리 배열 변수는 배열 데이터의 주소를 담고 있다.
+
         arrAtt2 = arrAtt1;
         System.out.println("arrAtt1: " + arrAtt1);
         System.out.println("arrAtt2: " + arrAtt2);
---
+++ src/Java02/MemoryOOP.java
@@ -0,0 +1,22 @@
+package Java02;
+
+public class MemoryOOP {
+
+
+
+
+    public static void main(String[] args) {
+
+        // 메모리에서 객체 생성 (동적생성)
+
+        /*
+        * 객체는 메모리에서 동적으로 생성되며,
+        * 객체가 더 이상 필요없게 되면
+        * Gabage Collector(GC)에 의해서 제거된다.
+        */
+
+
+    }
+
+
+}
