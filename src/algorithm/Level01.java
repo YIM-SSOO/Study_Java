9c81a66572d363b9af087e4aa3af54397f456bfa
YIM-SSOO <81502053+YIM-SSOO@users.noreply.github.com>
2021-10-04 00:01:50 +0900

    algorithm study

---
+++ src/algorithm/Level01.java
@@ -0,0 +1,17 @@
+package algorithm;
+
+public class Level01 {
+    public static void main(String[] args) {
+        // Escape Sequence
+        
+        // 1. 개 모양 출력
+
+        System.out.println("|\\_/|");
+        System.out.println("|q p|   /}");
+        System.out.println("( 0 )\"\"\"\\");
+        System.out.println("|\"^\"`    |");
+        System.out.println("||_/=\\\\__|");
+
+
+    }
+}
