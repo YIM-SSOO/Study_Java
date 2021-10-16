9b87fbfef69f97c53618ee9527e56da228075594
YIM-SSOO <81502053+YIM-SSOO@users.noreply.github.com>
2021-10-16 14:35:10 +0900

    Exception Throw.

--- src/Java02/ThrowsExam.java
+++ src/Java02/ThrowsExam.java
@@ -2,13 +2,24 @@ package Java02;
 
 public class ThrowsExam {
     public static void main(String[] args) {
+
+        // 정수에 값 넣기
         int i = 10;
         int j = 0;
-        int k = divide(i, j);
-        System.out.println(k);
+
+        // 오류를 처리해야하는 부분
+        try {
+            int k = divide(i, j);
+            System.out.println(k);
+        } catch (ArithmeticException e) {
+            // 오류의 정보르 출력
+            System.out.println(e.toString());
+        }
+
     }
 
-    public static int divide(int i, int j) {
+    // 정수 값 2개를 입력받고 그 정수를 나누어서 결과를 리턴하는 divide라는 메서드 정의
+    public static int divide(int i, int j) throws ArithmeticException, ClassCastException {
         int k = i / j;
         return k;
     }
