99cf800929d1c945de3f758dbd0bc09849de8d6a
YIM-SSOO <81502053+YIM-SSOO@users.noreply.github.com>
2021-10-21 23:57:48 +0900

    String exam add

---
+++ src/JavaStudy/day01/StringExam02.java
@@ -0,0 +1,21 @@
+package JavaStudy.day01;
+
+public class StringExam02 {
+
+    public static void main(String[] args) {
+        String str = "Hello,Elice!JavaProgramming";
+
+        //지시사항 1번을 참고하여 코드를 작성하세요.
+        System.out.println(str.length());
+
+
+        //지시사항 2번을 참고하여 코드를 작성하세요.
+        System.out.println(str.substring(12, str.length()));
+
+        //지시사항 3번을 참고하여 코드를 작성하세요.
+        System.out.println(str.toUpperCase());
+
+        //지시사항 4번을 참고하여 코드를 작성하세요.
+        System.out.println(str.toLowerCase());
+    }
+}
