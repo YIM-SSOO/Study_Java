22ebc3309069074fa9c6a553208e503cff9cee45
YIM-SSOO <81502053+YIM-SSOO@users.noreply.github.com>
2021-10-21 12:36:45 +0900

    String class add

--- src/java03/day03/StringExam.java
+++ src/java03/day03/StringExam.java
@@ -33,5 +33,11 @@ public class StringExam {
         String str6 = sb.toString();
         System.out.println(str6);
 
+
+       /*
+          두 경우 결과는 같지만 +연산을 한 경우에는 반복문 안에서 내부적으로 String 객체를 만들냅니다.
+          매번 new를 사용해서 연산을 해야하기 때문에 속도가 그만큼 느려지는 것이지요.
+          이러한 이유로 문자열을 반복문 안에서 더하는 것은 성능상 문제가 생길 수 있으니 반드시 피해야 합니다.
+       */
     }
 }
