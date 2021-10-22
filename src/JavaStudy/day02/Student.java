6e8b678598f624acf59463df0f44e9c86f8e37a9
YIM-SSOO <81502053+YIM-SSOO@users.noreply.github.com>
2021-10-22 22:52:03 +0900

    class exam

---
+++ src/JavaStudy/day02/Student.java
@@ -0,0 +1,14 @@
+package JavaStudy.day02;
+
+public class Student { //학생클래스
+
+
+    int number;  // 학번
+    int score;   //시험점수
+    String name; //이름
+
+    void study(){
+        System.out.println("Studying"); //공부하기
+    }
+
+}
